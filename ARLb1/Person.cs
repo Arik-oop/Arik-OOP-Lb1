97d4ba61aa4038222fe0436a4e62b30108c711ed
Arik-oop <abs17@tpu.ru>
2025-12-11 13:55:00 +0700

    feature ( Person ): реализован класс Person с валидацией
    - добавлен enum Gender для описания пола
    - реализован конструктор с проверкой возраста >= 0 и имени/фамилии != null
    - добавлен метод ToString для удобного отображения
    - реализована статическая фабрика GetRandomPerson для упрощения тестирования
    
    Закрыты #1

---
+++ ARLb1/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARLb1
+{
+    /// <summary>
+    /// Класс, описывающий персону: имя, фамилию, возраст и пол.
+    /// </summary>
+    public class Person
+    {
+        // Поля (можно использовать свойства напрямую — это более идиоматично в C#)
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Sex Sex { get; set; }
+
+        /// <summary>
+        /// Конструктор класса Person.
+        /// </summary>
+        /// <param name="name">Имя человека.</param>
+        /// <param name="surname">Фамилия человека.</param>
+        /// <param name="age">Возраст (должен быть >= 0).</param>
+        /// <param name="sex">Пол человека.</param>
+        /// <exception cref="ArgumentException">Выбрасывается, если возраст отрицательный.</exception>
+        public Person(string name, string surname, int age, Sex sex)
+        {
+            if (age < 0)
+                throw new ArgumentException("Возраст не может быть отрицательным.", nameof(age));
+
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Surname = surname ?? throw new ArgumentNullException(nameof(surname));
+            Age = age;
+            Sex = sex;
+        }
+
+        /// <summary>
+        /// Переопределение метода ToString для удобного отображения объекта.
+        /// </summary>
+        /// <returns>Строковое представление Person.</returns>
+        public override string ToString()
+        {
+            return $"{Name} {Surname}, {Age} лет, пол: {Sex}";
+        }
+    }
+}
--- ARLb1/Program.cs
+++ ARLb1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ARLb1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,29 @@ using System.Threading.Tasks;
 
 namespace ARLb1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                // Создаём двух персон
+                var person1 = new Person("Алексей", "Иванов", 25, Sex.Male);
+                var person2 = new Person("Мария", "Петрова", 30, Sex.Female);
+
+                // Выводим на экран
+                Console.WriteLine(person1);
+                Console.WriteLine(person2);
+
+                // Попытка создать персону с отрицательным возрастом (должна вызвать исключение)
+                var person3 = new Person("Тест", "Ошибка", -5, Sex.Male);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
+            Console.Read();
         }
     }
 }
